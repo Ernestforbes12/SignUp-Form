48ad5668ceea56d453a09d4fbba818e99773d463
Ernestforbes12 <104239785+Ernestforbes12@users.noreply.github.com>
2022-11-04 13:06:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//And the real pain begins-/-//
+
+const container = document.
+querySelector(".container");
+const signUpBtn = document.
+querySelector(".blue-bg button");
+
+signUpBtn.addEventListener('click', () => {
+ container.classList.toggle("change");
+});
